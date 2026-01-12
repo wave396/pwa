771f344bbadaa785d6303b45c859839a8807cbd9
wave396 <ludmilatkanova@gmail.com>
2026-01-12 12:08:15 +0200

    Final check for today

--- sw.js
+++ sw.js
@@ -85,9 +85,12 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
     "revision": "8e3a10e157f75ada21ab742c022d5430"
   }, {
     "url": "index.html",
-    "revision": "2beeb9e635e396d4bb7db068b27dbe06"
+    "revision": "aac696b1fc14199884e7009ce3b3c846"
   }, {
-    "url": "assets/index-DPNpWX40.js",
+    "url": "icon.png",
+    "revision": "970d4e042704544a83f135a78d646221"
+  }, {
+    "url": "assets/index-COTJfrrX.js",
     "revision": null
   }, {
     "url": "manifest.webmanifest",
